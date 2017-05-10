55a0180ef021e78c53a84bf6fad7ef7bf241e738
Simulation X <tyler@mccleve.me>
2017-05-09 20:28:18 -0600

    moved to rancher

--- vcloud/docker.tf
+++ vcloud/docker.tf
@@ -1,12 +1,11 @@
 resource "vcd_vapp" "docker-windows-test" {
   count         = 2
-  name          = "win-test-dev0${count.index}"
+  name          = "docker0${count.index}"
   catalog_name  = "docker"
-  template_name = "windows"
+  template_name = "rancher"
   memory        = 4048
   cpus          = 2
   network_name  = "${vcd_network.docker.name}"
   network_href	= "${vcd_network.docker.href}"
   ip 		= "172.17.0.1${count.index}"
 }
-
